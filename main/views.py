9729547a53a97ad8cbecf70026700ea989e5b25a
rafahazizah <rafahazizah@gmail.com>
2023-11-15 14:17:25 +0700

    redeploy

--- main/views.py
+++ main/views.py
@@ -116,7 +116,6 @@ def get_product_json(request):
     product_item = Product.objects.all()
     return HttpResponse(serializers.serialize('json', product_item))
 
-...
 @csrf_exempt
 def add_product_ajax(request):
     if request.method == 'POST':
@@ -131,3 +130,22 @@ def add_product_ajax(request):
         return HttpResponse(b"CREATED", status=201)
 
     return HttpResponseNotFound()
+
+@csrf_exempt
+def create_product_flutter(request):
+    if request.method == 'POST':
+        
+        data = json.loads(request.body)
+
+        new_product = Product.objects.create(
+            user = request.user,
+            name = data["name"],
+            price = int(data["price"]),
+            description = data["description"]
+        )
+
+        new_product.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
